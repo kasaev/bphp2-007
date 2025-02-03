79b0851138c22cfa23e80ed0f0da253ceb84f04f
Alexey Kasaev <kasaev@gmail.com>
2025-02-03 19:56:00 +0300

    Some updates

--- upload.php
+++ upload.php
@@ -1,44 +1,38 @@
 <?php
 declare(strict_types = 1);
 
-
+$errCount = 0;
 $redirectMessage = '';
 
-if ( isset($_POST['file_name']) ) {
+if ( isset($_POST['file_name']) && !empty($_POST['file_name'])) {
     $fileName = $_POST['file_name'];
-}
-
-
-
-if ( isset($_FILES['content']) ) {
-    $file = $_FILES['content'];
-}
-
-if ( $fileName === '') {
+} else {
+    $errCount++;
     $redirectMessage = $redirectMessage . 'Вы не ввели название файла\n';
 }
 
-if ( $file['size'] === 0 ) {
+if ( isset($_FILES['content']) && is_uploaded_file($_FILES['content']['tmp_name']) ) {
+    $file = $_FILES['content'];
+} else {
+    $errCount++;
     $redirectMessage = $redirectMessage . 'Вы не выбрали файл\n';
 }
 
-
-
-if ( $fileName !== '' && $file['size'] > 0 ) {
+if ($errCount === 0) {
     $upload_dir = $_SERVER['DOCUMENT_ROOT'] . '/upload';
 
     if ( !file_exists($upload_dir) ) {
         mkdir($upload_dir, 0777, true);
     }
 
-    if ( $file['error'] === 0 ) {
-        move_uploaded_file( $file['tmp_name'], $upload_dir . '/' . $fileName);
+    if ( move_uploaded_file( $file['tmp_name'], $upload_dir . '/' . $fileName) ) {
+        echo '<p>Вы успешно загрузили файл в ' . $upload_dir . '/' . $fileName . '</p>';
+        echo '<p>Размер файла: ' . $file['size'] . ' байт</p>';
+    } else {
+        echo '<p>При загрузке файла что-то пошло не так.</p>';
     }
-
-    echo '<p>Вы успешно загрузили файл в ' . $upload_dir . '/' . $fileName . '</p>';
-    echo '<p>Размер файла: ' . $file['size'] . ' байт</p>';
 } else {
-    header('Location:' . '/index.php'. '?message=' . urlencode($redirectMessage) . '&file_name = ' . ( isset($fileName) ? $fileName : '' ) . '&file=' . ( isset($file) ? $file : '' ));   
+    header('Location:' . '/index.php'. '?message=' . urlencode($redirectMessage) . '&file_name=' . $fileName ); 
 }
 
 
